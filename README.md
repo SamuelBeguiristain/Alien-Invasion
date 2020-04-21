a5f4960c8ac435c70813d137629b6f0edea45a1d
Samuel Beguiristain <a@sabe.dev>
2020-04-20 23:33:33 -0400

    Update 1
    Added images, updated alien_invasion, added settings, added ship (need to find a way to resize ship image)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Alien-Invasion
-Using Pygame to handle the craziness! ALIEN INVASION! 
+Using Pygame to handle the craziness! ALIEN INVASION!
+Not entirely my work, based off of Eric Matthes book Python Crash Course - 2nd Edition
---
+++ ship.py
@@ -0,0 +1,23 @@
+import pygame
+
+from settings import Settings
+
+class Ship:
+    # I was going to make the ship originally the poop emoji
+    """A class to image the \"SHIP\""""
+
+    def __init__(self, ai_game):
+        """Initalize the ship and set its starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/poop.bmp') # Attempting to resize some how
+        self.rect = self.image.get_rect()                 # using pygame.transform.scale()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
